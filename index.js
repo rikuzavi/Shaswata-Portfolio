dcd5a8fad86bf0473fa1b3ea107f3c48b424dd7d
SHASWATA  DAS <96969805+rikuzavi@users.noreply.github.com>
2025-11-16 18:37:17 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+document.getElementById('ed').addEventListener('click',()=>{
+    document.getElementById('frame').style.transform = 'translateX(-50%)'
+})
+document.getElementById('ts').addEventListener('click',()=>{
+    document.getElementById('frame').style.transform = 'translateX(0%)'
+})
+
+
+let darkmode = document.getElementById('darkmode')
+darkmode.addEventListener('click',()=>{
+    if(darkmode.children[0].classList[1] == 'fa-moon'){
+        darkmode.children[0].classList.replace(darkmode.children[0].classList[1],'fa-sun')
+        document.getElementsByTagName('body')[0].className = "bg-[bisque] duration-[0.5s]"
+        let twtext = document.querySelectorAll(".tw")
+        for(let i of twtext){
+            i.classList.replace(i.classList[0],'tb')
+        }
+    }else{
+        darkmode.children[0].classList.replace(darkmode.children[0].classList[1],'fa-moon')
+        document.getElementsByTagName('body')[0].className = "bg-[#313647] duration-[0.5s]"
+        let tbtext = document.querySelectorAll(".tb")
+        for(let i of tbtext){
+            i.classList.replace(i.classList[0],'tw')
+        }
+    }
+})
